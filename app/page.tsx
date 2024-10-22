0d5bba39ae0556ae788cd46afcebe93c77b5f027
teletobi <58395640+teletobe@users.noreply.github.com>
2024-10-22 21:58:16 +0200

    bug

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
 
     const loadRemainingImages = async () => {
       const remainingImages: any[] = [];
-      for (let i = 13; i <= 91; i++) {
+      for (let i = 14; i <= 91; i++) {
         try {
           const image = await import(`../public/pics/all/pf${i}.jpg`);
           remainingImages.push(image.default);
